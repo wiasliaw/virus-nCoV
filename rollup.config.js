83ea8812da5fbeba5b9b3081d341a421c1f4a0a5
Wei Lee <leewei48750@gmail.com>
2020-02-07 02:37:29 +0800

    feat(./): assest prefix for static page

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,34 @@ import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
+import html from '@rollup/plugin-html';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 
 const production = !process.env.ROLLUP_WATCH;
+const assetsPrefix = !!production ? '/virus-nCoV': '';
+
+const templateFunc = ({
+  title,
+  publicPath,
+}) => `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset='utf-8'>
+  <meta name='viewport' content='width=device-width,initial-scale=1'>
+
+  <title>${title}</title>
+
+  <link rel='icon' type='image/png' href='${publicPath}/favicon.png'>
+  <link rel='stylesheet' href='${publicPath}/global.css'>
+  <link rel='stylesheet' href='${publicPath}/build/bundle.css'>
+  <script defer src='${publicPath}/build/bundle.js'></script>
+</head>
+
+<body>
+</body>
+</html>
+`
 
 export default {
   input: 'src/main.js',
@@ -23,6 +47,11 @@ export default {
       }
     }),
     json(),
+    production && html({
+      publicPath: assetsPrefix,
+      title: 'nCoV-Graph',
+      template: templateFunc,
+    }),
 
     resolve({
       browser: true,
